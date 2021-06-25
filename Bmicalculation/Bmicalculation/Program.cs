30dbbcf85bd513ee366e0e9b3d8889ad6556556e
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-06-25 14:24:40 +0530

    Add files via upload

---
+++ Bmicalculation/Bmicalculation/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;namespace Bmicalculation{
+
+
+
+        class Program
+        {
+
+        static void Main(string[] args)        {            CheckMyBmi person = new CheckMyBmi();            person.ConsoleBmi();            person.CalculateBmi();
+
+
+        }    }}
