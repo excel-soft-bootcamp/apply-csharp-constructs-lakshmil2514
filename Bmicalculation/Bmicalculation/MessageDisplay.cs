176ce5f60b9c37997287d4063ae22f08bf2a9f77
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-06-26 13:47:29 +0530

    Add files via upload

---
+++ Bmicalculation/Bmicalculation/MessageDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bmicalculation
+{
+    class MessageDisplay
+    {
+        public void Underweight()
+        {
+            Console.WriteLine("Underweight");
+        }
+        public void Healthy()
+        {
+            Console.WriteLine("Healthy");
+        }
+        public void Overweight()
+        {
+            Console.WriteLine("Overweight");
+        }
+    }
+}
