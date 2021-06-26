b831c85b6ea6c91801e59ba10c5fd15a9857cc1a
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-06-26 15:05:06 +0530

    Add files via upload

---
+++ Inheritance/Inheritance/Collaege.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Inheritance
+
+{
+    class collage
+        {
+        private string ColgCode;
+      
+
+        public collage(double id)
+            
+        {
+            if (id=ColgCode)
+            {
+                id = 
+            }
+
+        }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Inheritance/Inheritance/teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class teacher : College
+    {
+
+        private string _teachername;
+        public teacher (string ID,string name):base (ID)
+        {
+            this._teachername = name;
+            public void subject()
+            {
+
+            }
+        }
+
+    }
+}
