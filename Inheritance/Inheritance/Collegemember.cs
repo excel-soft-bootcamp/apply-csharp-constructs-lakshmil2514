aff221bd5142a116285b64a33d923e5cd34147c4
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-06-26 21:34:34 +0530

    Add files via upload

---
+++ Inheritance/Inheritance/Collegemember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Inheritance
+
+{
+    class Collegemember
+    {
+        private string _ColgCode;
+        private string _Address;
+        public Collegemember(string Id, string address)
+        {
+
+            this._ColgCode = Id;
+            this._Address = address;
+        }
+
+    }
+}
