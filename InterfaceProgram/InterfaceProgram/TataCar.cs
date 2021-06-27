2691110136205948c1d7fd06c694576585634265
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-06-27 20:49:15 +0530

    Add files via upload

---
+++ InterfaceProgram/InterfaceProgram/TataCar.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceProgram
+{
+    public interface Iengine
+    {
+        void Start();
+        void Stop();
+
+    }
+    public class TataCar
+    {
+        Iengine _engine;
+
+        public TataCar(Iengine engine)
+        {
+
+            this._engine = engine;
+        }
+
+        public void Drive()
+        {
+
+            _engine.Start();
+        }
+        public void Halt()
+        {
+
+            _engine.Stop();
+        }
+
+
+    }
+   
+
+    public class DiCorEngine : Iengine
+    {
+
+        public void Start()
+        {
+           
+        }
+        public void Stop()
+        {
+           
+        }
+    }
+
+    public class VeriCorEngine : Iengine
+    {
+
+        public void Start()
+        {
+            
+        }
+        public void Stop()
+        {
+           
+        }
+    }
+
+    public class MultiJetEngine : Iengine
+    {
+
+        public void Start()
+        {
+           
+        }
+        public void Stop()
+        {
+
+        }
+
+    }
+    public class RevorTron : Iengine
+    {
+
+        public void Start()
+        {
+           
+        }
+        public void Stop()
+        {
+           
+        }
+    }
+}
+
+
+
