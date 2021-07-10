77b4bcc1bbef22bbede71356d2ea92fc266a26e2
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-07-10 09:41:35 +0530

    Add files via upload

---
+++ DoorSensorProgram/DoorSensorProgram/DoorSensorProgram/DoorSensorProgram/DoorSensor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSensorProgram
+{
+    public delegate void DoorStatusHandler(string status);
+    class DoorSensor
+    { 
+      
+        string status;
+
+        public event DoorStatusHandler OnDoorStatusChanged;
+        public void Open()
+        {
+            status = "Opened";
+            this.Notify();
+
+
+        }
+        public void Close()
+        {
+            status = "Closed";
+            this.Notify();
+
+        }
+        void Notify()
+        {
+           
+            OnDoorStatusChanged.Invoke(this.status);
+        }
+
+
+    }
+}
---
+++ DoorSensorProgram/DoorSensorProgram/DoorSensorProgram/DoorSensorProgram/SecuritySystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSensorProgram
+{
+    class SecuritySystem
+    {
+        public void Notify(string status)
+        {
+            string message = $"Door {status} and Time:{System.DateTime.Now.ToString()}";
+            Console.WriteLine(message);
+        }
+    }
+    
+}
