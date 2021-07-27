8491d7aedf68d9933f6c9e8caa8fdf401cebdaa2
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-07-27 12:08:55 +0530

    Delete Bmicalculation directory

--- Bmicalculation/Bmicalculation/Program.cs
+++
@@ -1,11 +0,0 @@
-﻿using System;namespace Bmicalculation{
-
-
-
-        class Program
-        {
-
-            static void Main(string[] args)            {            CheckMyBmi person = new CheckMyBmi();            person.ConsoleInput();            person.Validate();
-
-
-            }        }}
